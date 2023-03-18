0f155ff04188538063b572c0d5ac6806b641dfa0
Prince Narteh <prinarteh@gmail.com>
2023-03-18 18:34:30 +0000

    add constant value

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:getx_todo_list/app/modules/home/home_view.dart';
+import 'package:get/get.dart';
+import 'package:getx_todo_list/app/modules/home/home.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  await GetStorage.init();
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       title: "ToDo List using GetX",
       home: HomeScreen(),
     );
