26939d1cc7a702691355ce19b39b78723aa2b6e4
Prince Narteh <prinarteh@gmail.com>
2023-03-19 03:31:22 +0000

    add task provider

---
+++ lib/app/data/providers/tasks/provider.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:js_util';
+
+import 'package:get/get.dart';
+import 'package:getx_todo_list/app/core/utils/keys.dart';
+import 'package:getx_todo_list/app/data/models/task.dart';
+import 'package:getx_todo_list/app/data/services/storage/services.dart';
+
+class TaskProvider {
+  final StorageService _storageService = Get.find<StorageService>();
+
+  List<Task> readTask() {
+    var tasks = <Task>[];
+    jsonDecode(_storageService.read(taskKey).toString()).forEach(
+      (task) => tasks.add(Task.fromJson(task)),
+    );
+    return tasks;
+  }
+
+  void writeTask(List<Task> tasks) {
+    _storageService.write(taskKey, jsonEncode(tasks));
+  }
+}
